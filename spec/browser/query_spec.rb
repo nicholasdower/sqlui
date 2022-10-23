6932f2acce39fa05a50ffd2c80f24c4045718b68
Nick Dower <nicholasdower@gmail.com>
2022-10-23 16:16:52 +0200

    Add browser test for executing query at cursor.

--- spec/browser/query_spec.rb
+++ spec/browser/query_spec.rb
@@ -7,19 +7,30 @@ describe 'query' do
   let(:driver) { start_session }
   let(:wait) { Selenium::WebDriver::Wait.new(timeout: 5) }
 
-  shared_examples_for 'a simple query result' do
+  def wait_until_editor
+    wait.until do
+      element = driver.find_element(class: 'cm-content')
+      element if element.displayed?
+    end
+  end
+
+  def wait_until_results(*results)
+    header_elements = wait.until do
+      elements = driver.find_elements(css: '#result-box > table > thead > tr > th.cell')
+      elements if elements.size == results[0].size && elements.all?(&:displayed?)
+    end
+    headers = header_elements.map(&:text)
+    expect(headers).to eq(%w[id name description])
+    row_elements = wait.until { driver.find_elements(css: '#result-box > table > tbody > tr') }
+    rows = row_elements.map do |row_element|
+      row_element.find_elements(css: 'td.cell').map(&:text)
+    end
+    expect(rows).to eq(results)
+  end
+
+  shared_examples_for 'a query result' do |results|
     it 'loads expected results' do
-      header_elements = wait.until do
-        elements = driver.find_elements(css: '#result-box > table > thead > tr > th.cell')
-        elements if elements.size == 3 && elements.all?(&:displayed?)
-      end
-      headers = header_elements.map(&:text)
-      expect(headers).to eq(%w[id name description])
-      row_elements = wait.until { driver.find_elements(css: '#result-box > table > tbody > tr') }
-      rows = row_elements.map do |row_element|
-        row_element.find_elements(css: 'td.cell').map(&:text)
-      end
-      expect(rows).to eq([['1', 'Nick', 'A dev on the project.'], ['2', 'Laura', 'A supportive person.']])
+      wait_until_results(results)
     end
   end
 
@@ -28,20 +39,60 @@ describe 'query' do
       driver.get(url('/db/development/app?sql=select+id%2C+name%2C+description+from+names+order+by+id+limit+2%3B'))
     end
 
-    it_behaves_like 'a simple query result'
+    it 'loads expected results' do
+      wait_until_results(['1', 'Nick', 'A dev on the project.'], ['2', 'Laura', 'A supportive person.'])
+    end
   end
 
   context 'when sql specified via editor' do
     before do
       driver.get(url('/db/development/app'))
-      editor = wait.until do
-        element = driver.find_element(class: 'cm-content')
-        element if element.displayed?
-      end
+      editor = wait_until_editor
       editor.send_keys('select id, name, description from names order by id limit 2;')
       editor.send_keys(%i[control enter])
     end
 
-    it_behaves_like 'a simple query result'
+    it 'loads expected results' do
+      wait_until_results(['1', 'Nick', 'A dev on the project.'], ['2', 'Laura', 'A supportive person.'])
+    end
+  end
+
+  context 'when first of two editor queries executed via cursor position' do
+    before do
+      driver.get(url('/db/development/app'))
+      editor = wait_until_editor
+      editor.send_keys(
+        <<~SQL
+          select id, name, description from names where id = 1;
+
+          select id, name, description from names where id = 2;
+        SQL
+      )
+      editor.send_keys(%i[up up up])
+      editor.send_keys(%i[control enter])
+    end
+
+    it 'loads expected results' do
+      wait_until_results(['1', 'Nick', 'A dev on the project.'])
+    end
+  end
+
+  context 'when second of two editor queries executed via cursor position' do
+    before do
+      driver.get(url('/db/development/app'))
+      editor = wait_until_editor
+      editor.send_keys(
+        <<~SQL
+          select id, name, description from names where id = 1;
+
+          select id, name, description from names where id = 2;
+        SQL
+      )
+      editor.send_keys(%i[control enter])
+    end
+
+    it 'loads expected results' do
+      wait_until_results(['2', 'Laura', 'A supportive person.'])
+    end
   end
 end
