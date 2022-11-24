0d040ab7ec6bc1fdcaba30b88cad56749ba41193
Nick Dower <nicholasdower@gmail.com>
2022-11-24 16:53:08 +0100

    Link fix clipboard.js

--- client/clipboard.js
+++ client/clipboard.js
@@ -1,4 +1,4 @@
-export function copyTextToClipboard(text) {
+export function copyTextToClipboard (text) {
   const type = 'text/plain'
   const blob = new Blob([text], { type })
   navigator.clipboard.write([new window.ClipboardItem({ [type]: blob })])
